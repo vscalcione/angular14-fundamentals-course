64d2ff7c26bd72c1ba05845d72dac2e76f64dd4f
vscalcione <vincenzo99.scalcione@gmail.com>
2022-11-26 23:00:04 +0100

    Refactoring app.component file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myapp';
+  visible: boolean = true;
+  users = ['fabio', 'lorenzo', 'simone'];
+  url?: string | null;
+
+  clickHandler(event: MouseEvent) {
+    console.log(event);
+  }
+
+  inputHandler(event: Event) {
+    const target: HTMLInputElement = <HTMLInputElement>event.target;
+    console.log(target.value);
+  }
+
+  toggle() {
+    this.visible = !this.visible;
+  }
+
+  loadImage() {
+    this.url = 'https://angular.io/assets/images/logos/angular/angular.png';
+  }
+
+  unloadImage() {
+    this.url = null;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HandlersComponent } from './components/handlers/handlers.component';
-import { DirectivesComponent } from './components/directives/directives.component';
-import { SquareBracketsComponent } from './components/square-brackets/square-brackets.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HandlersComponent,
-    DirectivesComponent,
-    SquareBracketsComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
